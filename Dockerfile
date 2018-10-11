f03fdb2e11cf1674e5f80d30d1a7119de2920db1
Jorge Izquierdo <jorge@izqui.me>
2018-10-11 20:34:09 +0200

    Refactor config

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM node:8-onbuild
 
 ENV PORT 8080
 EXPOSE 8080
-ENV APMSERVE_RINKEBY_IPFS "ipfs.aragon.network"
-ENV HOST "aragonpm.com"
 ENV DOCKER_BUILD "true"
--- index.js
+++ index.js
@@ -1,23 +1,18 @@
+require('dotenv').config()
+
 const subdomain = require('express-subdomain')
 const express = require('express')
 const app = express()
 
 const apmRouter = require('./lib/apm-router')
 
-const networks = [
-  // TODO: As soon as we start using mainnet we should require rinkeby to be prefixed
-  { network: 'rinkeby', sub: '*' },
-  // { network: 'ropsten' },
-  // { network: 'kovan' },
-  // { network: 'rpc' },
-  // { network: 'mainnet', sub: '*' }, // wildcard, needs to be last
-]
+const networks = JSON.parse(process.env.APMSERVE_NETWORKS)
 
 app.use(require('cors')())
 
 // Always check hostname
 app.use((req, res, next) => {
-  req.basehost = process.env.HOST || 'aragonpm.test'
+  req.basehost = process.env.APMSERVE_HOST
 
   if (req.hostname.indexOf(req.basehost) === -1) {
     return next(new Error('Incorrect HOST name, please set HOST env variable correctly'))
@@ -40,5 +35,5 @@ app.use(function (err, req, res, next) {
 const port = process.env.PORT || 3000
 app.listen(port, (err) => {
   if (err) return console.error(err)
-  console.log('Listening on port', port)
+  console.log(`Listening on port ${port} (host: ${process.env.APMSERVE_HOST})`)
 })
